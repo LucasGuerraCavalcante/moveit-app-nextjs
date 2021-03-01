6a9f41169d76ad2cc848696c94f5a65b529090ec
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2021-03-01 14:24:44 -0300

    dinamic level variable

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContexts';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/LucasGuerraCavalcante.png" alt="Profile Image" />
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Lucas Guerra</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level Icon" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
